eee9fee9993b26b5a25f51c6447fa72c3cd893d5
SDWeb <SDWeb>
2021-11-03 21:18:16 -0300

    Criando operacao de instanciacao

--- src/main/java/com/daniellima/cursomc/domain/Categoria.java
+++ src/main/java/com/daniellima/cursomc/domain/Categoria.java
@@ -17,9 +17,15 @@ public class Categoria implements Serializable {
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
+
 	
-	public Categoria() {
-		
+
+	 
+
+	public Categoria(Integer id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
 	}
 
 	public Integer getId() {
