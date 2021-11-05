389099566a3d841d9bf32f23bdc537fc2b465524
Daniel Lima Lopes <daniel.lima.lopes@outlook.com>
2021-11-04 23:43:03 -0300

    Estado e cidade

--- src/main/java/com/daniellima/cursomc/CursomcApplication.java
+++ src/main/java/com/daniellima/cursomc/CursomcApplication.java
@@ -8,18 +8,29 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.daniellima.cursomc.domain.Categoria;
+import com.daniellima.cursomc.domain.Cidade;
+import com.daniellima.cursomc.domain.Estado;
 import com.daniellima.cursomc.domain.Produto;
 import com.daniellima.cursomc.repositorys.CategoriaRepository;
+import com.daniellima.cursomc.repositorys.CidadeRepository;
+import com.daniellima.cursomc.repositorys.EstadoRepository;
 import com.daniellima.cursomc.repositorys.ProdutoRepository;
 
+
+
 @SpringBootApplication
-public class CursomcApplication implements CommandLineRunner{
+public class CursomcApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
 	@Autowired
 	private ProdutoRepository produtoRepository;
-
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -28,27 +39,40 @@ public class CursomcApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 
-		//Instanciando os produtos e as categorias.
-		Categoria cat1= new Categoria(null,"Informática");
-		Categoria cat2= new Categoria(null,"Escritório");
+		// Instanciando os produtos e as categorias.
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
 
-		Produto p1 = new Produto(null,"Computador", 2000.00);
-		Produto p2 = new Produto(null,"Impressora", 800.00);
-		Produto p3 = new Produto(null,"Mouse", 80.00);
-		
-		//Adicionando os produtos que tem associacao com cada categoria.
-		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+
+		// Adicionando os produtos que tem associacao com cada categoria.
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
 		cat2.getProdutos().addAll(Arrays.asList(p2));
-		
-		//Adicionando as categorias que tem associacao com cada produto.
+
+		// Adicionando as categorias que tem associacao com cada produto.
 		p1.getCategoria().addAll(Arrays.asList(cat1));
 		p2.getCategoria().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategoria().addAll(Arrays.asList(cat1));
-		
-		
+
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 
+		Estado est1 = new Estado(null,"Minas Gerais");
+		Estado est2 = new Estado(null,"São Paulo");
+
+		Cidade c1 = new Cidade(null,"Uberlandia", est1);
+		Cidade c2 = new Cidade(null,"São Paulo",est2);
+		Cidade c3 = new Cidade(null,"Campinas",est1);
+
+		est1.getCidade().addAll(Arrays.asList(c1));
+		est2.getCidade().addAll(Arrays.asList(c2, c3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2,c3));
+		
+		
 
 	}
 
